44c2a00264048147f22726acadce2d632ec9850a
Emanuele Canessa <ema0185@gmail.com>
2015-03-11 21:28:52 +0100

    Prima lezione

--- app/src/main/java/com/example/emanuele/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/emanuele/firstandroidapp/MainActivity.java
@@ -1,17 +1,42 @@
 package com.example.emanuele.firstandroidapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView attempts;
+    private EditText username = null;
+    private EditText password = null;
+    private Button login_button;
+    private int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText)findViewById(R.id.editText);
+        password = (EditText)findViewById(R.id.editText2);
+        attempts = (TextView)findViewById(R.id.textView4);
+        attempts.setText(getString(R.string.attempts) + " " + Integer.toString(counter));
+        login_button = (Button)findViewById(R.id.button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(getCurrentFocus());
+            }
+        });
     }
 
 
@@ -35,5 +60,23 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login (View view){
+        String _username = username.getText().toString();
+        String _password = password.getText().toString();
+
+        if (_username.equals("admin") && _password.equals("admin")){
+            Toast.makeText(getApplicationContext(), getString(R.string.redirecting), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), getString(R.string.auth_fail), Toast.LENGTH_SHORT).show();
+            counter--;
+            attempts.setText(getString(R.string.attempts) +  " " + counter);
+            if (counter == 0){
+                attempts.setTextColor(Color.RED);
+                login_button.setEnabled(false);
+            }
+        }
     }
 }
